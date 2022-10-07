66cc49cf97e9de31fd302bb40d1a454597ceac7c
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2022-10-07 02:43:31 +0200

    streaming

--- services/auth/apps/users/models.py
+++ services/auth/apps/users/models.py
@@ -2,6 +2,10 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from apps import kafka_util
 
 
 class User(AbstractUser):
@@ -27,3 +31,20 @@ class User(AbstractUser):
         if name:
             return f"{self.username} ({name})"
         return self.username
+
+
+@receiver(post_save, sender=User, dispatch_uid="user_streaming")
+def send_comment_to_client(instance: User, **kwargs):
+    kafka_util.send_message(
+        "user-streaming",
+        {
+            param: str(getattr(instance, param)) if getattr(instance, param) else None
+            for param in [
+                "public_id",
+                "username",
+                "role",
+                "first_name",
+                "last_name",
+            ]
+        },
+    )
