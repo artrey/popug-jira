61ddcaefbcb09560162017aa7b63f6504519d43b
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2022-10-16 12:03:38 +0200

    fix auth

--- services/auth/apps/users/models.py
+++ services/auth/apps/users/models.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-
-import apps.kafka_util.proto as kafka_proto
-from apps import kafka_util
+from kafka_util import producer
 
 
 class User(AbstractUser):
@@ -36,34 +34,28 @@ class User(AbstractUser):
 
 @receiver(post_save, sender=User, dispatch_uid="user_streaming_create_update")
 def user_streaming_create_update(instance: User, created: bool, **kwargs):
-    kafka_util.send_message(
-        "user-streaming",
-        kafka_proto.Message(
-            entity="user",
-            event=kafka_proto.EventType.CREATE if created else kafka_proto.EventType.UPDATE,
-            public_id=str(instance.public_id),
-            data=kafka_proto.User(
-                **{
-                    param: str(getattr(instance, param, ""))
-                    for param in [
-                        "username",
-                        "role",
-                        "first_name",
-                        "last_name",
-                    ]
-                }
-            ),
-        ),
+    producer.send_event(
+        "user-stream",
+        {
+            param: str(getattr(instance, param, ""))
+            for param in [
+                "public_id",
+                "username",
+                "role",
+                "first_name",
+                "last_name",
+            ]
+        },
+        "auth.UserRegistered" if created else "auth.UserUpdated",
+        1,
     )
 
 
 @receiver(post_delete, sender=User, dispatch_uid="user_streaming_delete")
 def user_streaming_delete(instance: User, **kwargs):
-    kafka_util.send_message(
-        "user-streaming",
-        kafka_proto.Message(
-            entity="user",
-            event=kafka_proto.EventType.DELETE,
-            public_id=str(instance.public_id),
-        ),
+    producer.send_event(
+        "user-stream",
+        {"public_id": str(instance.public_id)},
+        "auth.UserDeleted",
+        1,
     )
