5b3d8003c8340fa22f4d5c5d8ff0a98fc8994fdf
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2022-10-16 12:40:10 +0200

    updated scheme registry

--- services/lib/scheme_registry/schemes_generator/auth.py
+++ services/lib/scheme_registry/schemes_generator/auth.py
@@ -2,6 +2,8 @@ import enum
 
 from pydantic import BaseModel
 
+from schemes_generator import BaseEventV1
+
 
 class UserRoleEnumV1(enum.Enum):
     ADMIN = "Администратор"
@@ -10,9 +12,13 @@ class UserRoleEnumV1(enum.Enum):
     POPUG = "Попуг"
 
 
-class UserRegisteredV1(BaseModel):
+class UserRegisteredDataV1(BaseModel):
     public_id: str
     username: str
     first_name: str
     last_name: str
     role: UserRoleEnumV1
+
+
+class UserRegisteredV1(BaseEventV1[UserRegisteredDataV1]):
+    pass
--- services/lib/scheme_registry/schemes_generator/task_tracker.py
+++ services/lib/scheme_registry/schemes_generator/task_tracker.py
@@ -3,16 +3,22 @@ import enum
 
 from pydantic import BaseModel
 
+from schemes_generator import BaseEventV1
+
 
 class TaskStatusEnumV1(enum.Enum):
     IN_PROGRESS = "IN_PROGRESS"
     COMPLETED = "COMPLETED"
 
 
-class TaskCreatedV1(BaseModel):
+class TaskCreatedDataV1(BaseModel):
     public_id: str
     title: str
     status: TaskStatusEnumV1
     executor_public_id: str
     created_at: dt.datetime
     updated_at: dt.datetime
+
+
+class TaskCreatedV1(BaseEventV1[TaskCreatedDataV1]):
+    pass
